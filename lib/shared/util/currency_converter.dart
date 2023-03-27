340fc8046c9a95c7e37784f962d5f81377346d82
nuresti <37360942+nuresti@users.noreply.github.com>
2023-03-27 14:53:31 +0700

    dart docs converter currency

--- lib/shared/util/currency_converter.dart
+++ lib/shared/util/currency_converter.dart
@@ -1,6 +1,10 @@
 import 'package:intl/intl.dart';
 
+/// Class untuk mengkonversi nilai numerik menjadi format mata uang Indonesia.
 class CurrencyConverter {
+  /// Mengembalikan string format mata uang Indonesia dari [value].
+  /// [value] harus berupa numerik.
+  /// Format yang digunakan adalah 'Rp ###,###.##-'.
   static format(num value) {
     String price = NumberFormat.currency(
             locale: 'id', decimalDigits: 2, customPattern: 'Rp ###,###.##-')
